895b711361608f9e5f1e3a20bd96adbb3dbb9204
hoatre <thanh_hoavn85@gmail.com>
2015-01-21 16:20:15 -0800

    Comiit

--- src/main/java/storm/tpb/topology/PARAM.java
+++ src/main/java/storm/tpb/topology/PARAM.java
@@ -27,4 +27,17 @@ public class PARAM implements Serializable  {
             return value;
         }
     }
+
+    public static enum Time {
+        MILLISECONDS(1), SECONDS(1000), MINUTES(SECONDS.getTime() *
+                60), HOURS(MINUTES.getTime() * 60), DAYS(HOURS
+                .getTime() * 24), WEEKS(DAYS.getTime() * 7);
+        private long millis;
+        private Time(long millis) {
+            this.millis = millis;
+        }
+        public long getTime() {
+            return this.millis;
+        }
+    }
 }
