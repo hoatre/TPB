960b008326eca4bdf33ffdc1b02fd4b6c44a6d1f
phonghh2 <phonghh1989@gmail.com>
2015-01-28 20:21:28 -0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/java/storm/tpb/topology/TopologyControl.java

--- src/main/java/storm/tpb/testing/SaveRedisForChart.java
+++ src/main/java/storm/tpb/testing/SaveRedisForChart.java
@@ -7,16 +7,23 @@ import storm.trident.operation.BaseFunction;
 import storm.trident.operation.TridentCollector;
 import storm.trident.tuple.TridentTuple;
 
+import java.net.SocketTimeoutException;
+
 /**
  * Created by quangnb on 1/22/15.
  */
 public class SaveRedisForChart extends BaseFunction {
     private Jedis jedis;
     public synchronized void execute(TridentTuple tuple, TridentCollector collector) {
-        jedis=new Jedis(Properties.getString("redis.host"), Properties.getInt("redis.port"), 2000);
-        jedis.set("real-time-" + PARAM.Channel.BRANCH1.getValue() + "-" + Long.toString(tuple.getLongByField("window")), Long.toString(tuple.getLongByField("countBranch1")));
-        jedis.set("real-time-" + PARAM.Channel.BRANCH2.getValue() + "-" + Long.toString(tuple.getLongByField("window")), Long.toString(tuple.getLongByField("countBranch2")));
-        jedis.set("real-time-" + PARAM.Channel.BRANCH3.getValue() + "-" + Long.toString(tuple.getLongByField("window")), Long.toString(tuple.getLongByField("countBranch3")));
-        jedis.set("real-time-" + PARAM.Channel.BRANCH4.getValue() + "-" + Long.toString(tuple.getLongByField("window")), Long.toString(tuple.getLongByField("countCenter")));
+        try {
+            jedis = new Jedis(Properties.getString("redis.host"), Properties.getInt("redis.port"), 2000);
+            jedis.set("real-time-" + PARAM.Channel.BRANCH1.getValue() + "-" + Long.toString(tuple.getLongByField("window")), Long.toString(tuple.getLongByField("countBranch1")));
+            jedis.set("real-time-" + PARAM.Channel.BRANCH2.getValue() + "-" + Long.toString(tuple.getLongByField("window")), Long.toString(tuple.getLongByField("countBranch2")));
+            jedis.set("real-time-" + PARAM.Channel.BRANCH3.getValue() + "-" + Long.toString(tuple.getLongByField("window")), Long.toString(tuple.getLongByField("countBranch3")));
+            jedis.set("real-time-" + PARAM.Channel.BRANCH4.getValue() + "-" + Long.toString(tuple.getLongByField("window")), Long.toString(tuple.getLongByField("countCenter")));
+        }catch (Exception e)
+        {
+
+        }
     }
 }
--- src/main/java/storm/tpb/testing/SaveRedisTopBotBolt.java
+++ src/main/java/storm/tpb/testing/SaveRedisTopBotBolt.java
@@ -21,36 +21,34 @@ public class SaveRedisTopBotBolt extends BaseFunction {
         this.TranType = TranType;
     }
     public synchronized void execute(TridentTuple tuple, TridentCollector collector) {
-        jedis=new Jedis(Properties.getString("redis.host"), Properties.getInt("redis.port"), 2000);
+        try {
+            jedis = new Jedis(Properties.getString("redis.host"), Properties.getInt("redis.port"), 2000);
 
-        if(!tuple.isEmpty())
-        {
-            for(int z = 1; z <=5 ; z++)
-            {
-                jedis.hdel("TopTen" + this.TranType + "-Top" + Integer.toString(z) + "-" + Long.toString(tuple.getLongByField("window")), "Acc","Amount");
-                jedis.hdel("TopTen" + this.TranType + "-Bot" + Integer.toString(z) + "-" + Long.toString(tuple.getLongByField("window")), "Acc","Amount");
-            }
-            List<String> TopFive = (ArrayList<String>)tuple.get(0);
-            if(TopFive != null)
-            {
-                for(int i = 0; i < TopFive.size(); i++) {
-                    Map<String, String> map = new HashMap<String, String>();
-                    map.put("Acc", TopFive.get(i).substring(0, TopFive.get(i).indexOf(",")));
-                    map.put("Amount", TopFive.get(i).substring(TopFive.get(i).lastIndexOf(",") + 1));
-                    jedis.hmset("TopTen" + this.TranType + "-Top" + Integer.toString(i + 1) + "-" + Long.toString(tuple.getLongByField("window")), map);
+            if (!tuple.isEmpty()) {
+                for (int z = 1; z <= 5; z++) {
+                    jedis.hdel("TopTen" + this.TranType + "-Top" + Integer.toString(z) + "-" + Long.toString(tuple.getLongByField("window")), "Acc", "Amount");
+                    jedis.hdel("TopTen" + this.TranType + "-Bot" + Integer.toString(z) + "-" + Long.toString(tuple.getLongByField("window")), "Acc", "Amount");
+                }
+                List<String> TopFive = (ArrayList<String>) tuple.get(0);
+                if (TopFive != null) {
+                    for (int i = 0; i < TopFive.size(); i++) {
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("Acc", TopFive.get(i).substring(0, TopFive.get(i).indexOf(",")));
+                        map.put("Amount", TopFive.get(i).substring(TopFive.get(i).lastIndexOf(",") + 1));
+                        jedis.hmset("TopTen" + this.TranType + "-Top" + Integer.toString(i + 1) + "-" + Long.toString(tuple.getLongByField("window")), map);
+                    }
+                }
+                List<String> BotFive = (ArrayList<String>) tuple.get(1);
+                if (BotFive != null) {
+                    for (int i = 0; i < BotFive.size(); i++) {
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("Acc", BotFive.get(i).substring(0, BotFive.get(i).indexOf(",")));
+                        map.put("Amount", BotFive.get(i).substring(BotFive.get(i).lastIndexOf(",") + 1));
+                        jedis.hmset("TopTen" + this.TranType + "-Bot" + Integer.toString(i + 1) + "-" + Long.toString(tuple.getLongByField("window")), map);
+                    }
                 }
             }
-            List<String> BotFive = (ArrayList<String>)tuple.get(1);
-            if(BotFive != null)
-            {
-                for(int i = 0; i < BotFive.size(); i++) {
-                    Map<String, String> map = new HashMap<String, String>();
-                    map.put("Acc", BotFive.get(i).substring(0, BotFive.get(i).indexOf(",")));
-                    map.put("Amount", BotFive.get(i).substring(BotFive.get(i).lastIndexOf(",") + 1));
-                    jedis.hmset("TopTen" + this.TranType + "-Bot" + Integer.toString(i + 1) + "-" + Long.toString(tuple.getLongByField("window")), map);
-                }
-            }
-        }
+        }catch (Exception e){}
 
     }
 }
